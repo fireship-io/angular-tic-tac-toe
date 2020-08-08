d4a27e07c5966c37137d12afea75831596c5e9f2
Balaguru4580 <balaguru4580@gmail.com>
2020-08-08 11:42:53 +0530

    Post win issue solved

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,14 +3,16 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button mat-flat-button color="primary" *ngIf="!value">{{ value }}</button>
+    <button mat-flat-button color="primary" *ngIf="!value && (flag ==  null)">{{ value }}</button>
+    <button mat-flat-button [disabled]="flag" color="primary" *ngIf="flag && (value == null)">{{ value }}</button>
     <button mat-flat-button color = "warn" *ngIf="value == 'X'">{{ value }}</button>
     <button mat-raised-button color = "primary" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important;}',]
+  styles: ['button { width: 100%; height: 100%; font-size: 2.5em !important;}',]
 })
 export class SquareComponent  {
 
   @Input() value: 'X' | 'O';
+  @Input() flag: string;
 
 }
