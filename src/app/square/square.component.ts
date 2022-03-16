cd2e4c3ca63cc7b24699478eac882f914efdbf6e
Eric <eaj1990@outlook.com>
2022-03-15 22:03:30 -0400

    remove outline and box shadow after selection

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; outline: none !important; box-shadow: none !important  }']
 })
 export class SquareComponent  {
 
